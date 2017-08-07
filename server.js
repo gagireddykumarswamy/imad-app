a52c548260b10038b4aacadf7502885c2f77d53f
gagireddykumarswamy <30691573+gagireddykumarswamy@users.noreply.github.com>
2017-08-07 16:18:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,14 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/artical-one',function(req,res){
+    res.send("arical one is requested");
+});
+
+app.get('/artical-two',function(req,res){
+   res.send("artical two is requested"); 
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
