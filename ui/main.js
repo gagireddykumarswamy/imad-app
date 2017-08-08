5e574acb4fbb717d88f97064b8cd4f48065aa38d
gagireddykumarswamy <30691573+gagireddykumarswamy@users.noreply.github.com>
2017-08-08 17:39:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,6 +20,20 @@ element.onclick = function(){
   request.send(null);
 };
 
+var inputname=document.getElementById('name');
+var inputvalue=inputname.value;
+var submit=document.getElementById('submit_btn');
+submit.onclick = function(){
+    
+var names=['name1','name2','name3'];    
+  var list;  
+  for(var i=0;i<names.length;i++){
+      
+      list += '<li>' + names[i] + '</li>';
+  }
+  var ul=document.getElementById('namelist');
+  ul.innerHTML=list;
+};
 /*var img=document.getElementById('kumar');
 var marginLeft = 0;
 function moveRight(){
