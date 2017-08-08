fa4a6e645c602387fed27ea08ed1a581f1a53e90
gagireddykumarswamy <30691573+gagireddykumarswamy@users.noreply.github.com>
2017-08-08 11:02:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,13 @@
 console.log('Loaded!');
 
 var img=document.getElementById('kumar');
+var marginleft = 0;
+function moveRight(){
+  marginLeft=marginLeft+10;
+  img.style.marginLeft=marginleft+'px';
+}
 
 img.onclick = function()
 {
-  img.style.marginLeft ='100px';  
+ var interval=setInterval(moveRight,100);  
 };
