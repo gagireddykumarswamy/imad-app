f02a8436fb25b362ac5a44ed6fb6e9567818289b
gagireddykumarswamy <30691573+gagireddykumarswamy@users.noreply.github.com>
2017-08-08 17:41:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,8 +26,8 @@ var submit=document.getElementById('submit_btn');
 submit.onclick = function(){
     
 var names=['name1','name2','name3'];    
-  var list;  
-  for(var i=0;i<names.length;i++){
+  var list='';  
+  for(var i=0; i<names.length; i++){
       
       list += '<li>' + names[i] + '</li>';
   }
