bf696255e409dc4a102a2701123373f33527161f
gagireddykumarswamy <30691573+gagireddykumarswamy@users.noreply.github.com>
2017-08-09 10:00:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,19 +20,33 @@ element.onclick = function(){
   request.send(null);
 };
 
-var inputname=document.getElementById('name');
-var inputvalue=inputname.value;
+
 var submit=document.getElementById('submit_btn');
 submit.onclick = function(){
+        var request=new XMLHttpRequest();
     
-var names=['name1','name2','name3'];    
-  var list='';  
-  for(var i=0; i<names.length; i++){
+    request.onreadystatechange = function(){
+      if(request.readyState === XMLHttpRequest.DONE){
+          if(request.status === 200){
+            var names=request.responseTesx;  
+            names=JSON.parse(names);
+            var list='';  
+              for(var i=0; i<names.length; i++){
       
-      list += '<li>' + names[i] + '</li>';
-  }
-  var ul=document.getElementById('namelist');
-  ul.innerHTML=list;
+              list += '<li>' + names[i] + '</li>';
+                }
+          var ul=document.getElementById('namelist');
+          ul.innerHTML=list; 
+          }
+          
+      }  
+        
+    };
+    var inputname=document.getElementById('name');
+var inputvalue=inputname.value;
+    request.open('GET','http://gagireddykumarswamy.imad.hasura-app.io/submit-name?name' + name,true);
+  request.send(null);
+
 };
 /*var img=document.getElementById('kumar');
 var marginLeft = 0;
