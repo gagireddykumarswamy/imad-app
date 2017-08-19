67f9f57952707707777cd9c14319b66e820ba767
gagireddykumarswamy <30691573+gagireddykumarswamy@users.noreply.github.com>
2017-08-19 21:06:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var Pool=require('pg').Pool;
 var config={
   user:'gagireddykumarswamy',
   database:'gagireddykumarswamy',
-  host:'http://db.imad.hasura-app.io',
+  host:'db.imad.hasura-app.io',
   port:'5432',
   password:process.env.DB_PASSWORD
   
