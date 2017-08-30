6d93e1d69ea0244a890c94c5010a388de6519af0
gagireddykumarswamy <30691573+gagireddykumarswamy@users.noreply.github.com>
2017-08-30 12:32:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool=require('pg').Pool;
 var crypto=require('crypto');
+var bodyParser=require('body-parser');
+
 var config={
   user:'gagireddykumarswamy',
   database:'gagireddykumarswamy',
@@ -14,6 +16,7 @@ var config={
 
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.json());
 
 var articalOne = {
     title:'artical one',
@@ -117,6 +120,23 @@ app.get('/hash/:input',function(req,res){
     
     var hashedString=hash(req.params.input,'this-is-random-string');
     res.send(hashedString);
+});
+
+app.post('/create-user',function(req,res){
+    var username=req.body.username;
+    var password=req.body.password;
+    
+    var salt=crypto.getRandomBytes(128).tostring('hex');
+    var dbstring=hash(password,salt);
+    pool.query('insert into "user" (username,password) values ($1,$2)',[username,dbstring],function(err,result){
+        
+        if(err){
+          res.status(500).send(err.toString());
+      } else {
+          res.send("user successfully created"+ username);
+      }
+    });
+    
 });
 
 app.get('/artical-tw0',function(req,res){
