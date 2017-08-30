7f46977e4f51d532a066c4aa8d409697cee46731
gagireddykumarswamy <30691573+gagireddykumarswamy@users.noreply.github.com>
2017-08-30 17:22:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -151,12 +151,12 @@ app.post('/login',function(req,res){
                 res.send(403).send("username and pasword not valid");
             }else{
                 var dbString=result.roes[0].password;
-                var salt=dbString.split("$")[2];
+                var salt=dbString.split('$')[2];
                 var hashedPassword=hash(password,salt);
                 if(hashedPassword === dbString){
-                    res.send("credentials are correct");
+                    res.send('credentials are correct');
                 }else{
-                    res.send(403).send("username and password invalid");
+                    res.send(403).send('username and password invalid');
                 }
                 
             }
